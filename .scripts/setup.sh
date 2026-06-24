53cfc5179949626b238ea0613e7e72affd8c806d
Adelino Lourenço <adelino.lourenco@xvia.com.br>
2026-06-24 14:07:54 -0300

    chore: fix golang version for docker containers

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -4,9 +4,9 @@ PROJECT_NAME="github.com/AdelinoLSN/pokemon-availability"
 
 echo "Starting..."
 
-docker run --rm -v "$PWD":/app -w /app golang:1.26-alpine go mod init "$PROJECT_NAME" || echo "go.mod already exists."
+docker run --rm -v "$PWD":/app -w /app golang:1.26.3-alpine go mod init "$PROJECT_NAME" || echo "go.mod already exists."
 
-docker run --rm -v "$PWD":/app -w /app golang:1.26-alpine sh -c "go mod tidy"
+docker run --rm -v "$PWD":/app -w /app golang:1.26.3-alpine sh -c "go mod tidy"
 
 echo "Files go.mod and go.sum are ready!"
 echo "Now you can run: docker-compose up --build"
