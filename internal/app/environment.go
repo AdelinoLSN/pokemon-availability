d6f056629e18c891f6bdf16661902b9fa911eb64
Adelino Lourenço <adelino.lourenco@xvia.com.br>
2026-06-24 14:08:15 -0300

    feat: add more information about loading environment variables

--- internal/app/environment.go
+++ internal/app/environment.go
@@ -8,10 +8,11 @@ import (
 
 func LoadEnvironmentVariables() error {
 	if err := godotenv.Load(); err != nil {
+		log.Default().Println("Failed to load .env file: using system environment variables")
 		return err
 	}
 
-	log.Default().Println("Loaded environment variables")
+	log.Default().Println("Loaded environment variables from .env file")
 
 	return nil
 }
